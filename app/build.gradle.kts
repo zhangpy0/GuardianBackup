53bd808e26cb9747d26741d2a6adb88dcdd6c900
张鹏宇 <1055803945@qq.com>
2025-12-03 17:26:29 +0800

    update 删除C++部分

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    externalNativeBuild {
-        cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
-        }
-    }
+//    externalNativeBuild {
+//        cmake {
+//            path = file("src/main/cpp/CMakeLists.txt")
+//            version = "3.22.1"
+//        }
+//    }
     buildFeatures {
         viewBinding = true
     }
